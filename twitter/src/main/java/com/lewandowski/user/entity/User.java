5437d23faa8e800dbe947dab7d012e17149a3aa4
Michal Lewandowski <Michal.Lewandowski@solarwinds.com>
2017-05-13 11:36:57 +0200

    User entity - temp

--- twitter/src/main/java/com/lewandowski/user/entity/User.java
+++ twitter/src/main/java/com/lewandowski/user/entity/User.java
@@ -1,5 +1,35 @@
 package com.lewandowski.user.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
+    @Size(min = 3, max = 12)
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
