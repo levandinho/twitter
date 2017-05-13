5f118ef1656d1b03df255f5be5eaf1466a831a90
Michal Lewandowski <Michal.Lewandowski@solarwinds.com>
2017-05-13 10:50:25 +0200

    Initial project structure - created from archetype

---
+++ twitter/src/main/java/com/lewandowski/App.java
@@ -0,0 +1,13 @@
+package com.lewandowski;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
